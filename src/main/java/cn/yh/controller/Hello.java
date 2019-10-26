969e5d602b3d5fee5c73eb9a96c5e03151bb0ee1
zhouzhoandhuihui <17805801525@163.com>
2019-10-26 15:51:37 +0800

    init

---
+++ src/main/java/cn/yh/controller/Hello.java
@@ -0,0 +1,15 @@
+package cn.yh.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @RequestMapping("/hello")
+    public String  hello(@RequestParam String name){
+        return "hello "+name+"，this is first messge";
+    }
+
+}
